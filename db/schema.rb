6193afbcb95fa6bd7142de3f466f6ec759736ce6
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2016-09-14 15:06:05 +0800

    Commit schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 20160913101853) do
 
   create_table "messages", force: :cascade do |t|
-    t.string   "name"
-    t.text     "message"
+    t.string   "user"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
