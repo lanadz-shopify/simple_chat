0e461a56eff1faa232886fa427667e81a773693b
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2016-09-14 14:36:09 +0800

    Rename message attributes

--- db/migrate/20160913101853_create_messages.rb
+++ db/migrate/20160913101853_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.string :name
-      t.text :message
+      t.string :user
+      t.text :body
       t.timestamps
     end
   end
